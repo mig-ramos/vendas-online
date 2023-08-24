acbeb9807632fb72f83c7c787eb61cc5acfbab79
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-08-24 18:06:02 -0300

    feat: create city controller and insert cache

--- backend/src/city/city.module.ts
+++ backend/src/city/city.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityController } from './city.controller';
+import { CityService } from './city.service';
+import { CityEntity } from './entities/city.entity';
 
-@Module({})
+@Module({
+  imports: [
+    CacheModule.register({
+      ttl: 900000000, // em milisegundos
+    }),
+    TypeOrmModule.forFeature([CityEntity]),
+  ],
+  controllers: [CityController],
+  providers: [CityService],
+})
 export class CityModule {}
