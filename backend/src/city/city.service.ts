81569aa20e6b2159c622169a9064a0b1107650e0
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-08-26 00:03:00 -0300

    feat: insert validation in address

--- backend/src/city/city.service.ts
+++ backend/src/city/city.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CacheService } from 'src/cache/cache.service';
 import { Repository } from 'typeorm';
@@ -9,6 +9,7 @@ export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+
     private readonly cacheService: CacheService,
   ) {}
 
@@ -20,5 +21,19 @@ export class CityService {
         },
       }),
     );
+  }
+
+  async findCityById(cityId: number): Promise<CityEntity> {
+    const city = await this.cityRepository.findOne({
+      where: {
+        id: cityId,
+      },
+    });
+
+    if (!city) {
+      throw new NotFoundException(`CityId: ${cityId} not found.`);
+    }
+
+    return city;
   }
 }
