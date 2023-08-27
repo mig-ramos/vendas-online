d3eb5f8602d3ec432d981f94ee9615363725b909
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-08-27 00:56:26 -0300

    feat: create unit test in userService

--- backend/src/address/dtos/returnAddress.dto.ts
+++ backend/src/address/dtos/returnAddress.dto.ts
@@ -1,4 +1,4 @@
-import { ReturnCityDto } from 'src/city/dtos/rreturnCity.dto';
+import { ReturnCityDto } from '../../city/dtos/rreturnCity.dto';
 import { AddressEntity } from '../entities/address.entity';
 
 export class ReturnAddressDto {
