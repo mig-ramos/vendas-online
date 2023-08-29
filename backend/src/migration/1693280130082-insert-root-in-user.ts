716e272dd8865741fd34a2ee5992d38e755b492d
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-08-29 01:10:43 -0300

    feat: create productService

---
+++ backend/src/migration/1693280130082-insert-root-in-user.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class insertRootInUser1675770516768 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            INSERT INTO public."user"(
+                name, email, cpf, type_user, phone, password)
+                VALUES ('root', 'root@root.com', '12345678901', 2, '31925325252', '$2b$10$x5o.9DD17Q5G2yq8k6JZKu9w1ovR.1ra0LitNUDZLGi0/z1FdfpfK');
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+            DELETE FROM public."user"
+                WHERE email like 'root@root.com';
+        `);
+  }
+}
