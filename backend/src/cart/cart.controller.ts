f0ef4e1f65f3ceb2829b2653fd54745735950285
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-09-01 23:59:25 -0300

    feat: clear cart

--- backend/src/cart/cart.controller.ts
+++ backend/src/cart/cart.controller.ts
@@ -1,6 +1,8 @@
 import {
   Body,
   Controller,
+  Delete,
+  Get,
   Post,
   UsePipes,
   ValidationPipe,
@@ -8,6 +10,7 @@ import {
 import { Roles } from '../decorators/roles.decorator';
 import { UserId } from 'src/decorators/user-id.decorator';
 import { UserType } from '../user/enum/user-type.enum';
+import { DeleteResult } from 'typeorm';
 import { CartService } from './cart.service';
 import { InsertCartDTO } from './dtos/insert-cart.dto';
 import { ReturnCartDTO } from './dtos/return-cart.dto';
@@ -26,5 +29,17 @@ export class CartController {
     return new ReturnCartDTO(
       await this.cartService.insertProductInCart(insertCart, userId),
     );
+  }
+
+  @Get()
+  async findCartByUserId(@UserId() userId: number): Promise<ReturnCartDTO> {
+    return new ReturnCartDTO(
+      await this.cartService.findCartByUserId(userId, true),
+    );
+  }
+
+  @Delete()
+  async clearCart(@UserId() userId: number): Promise<DeleteResult> {
+    return this.cartService.clearCart(userId);
   }
 }
