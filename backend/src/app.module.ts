76a0d6233c8093296ffc558c25549e8a3f9adbc8
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-09-02 22:12:12 -0300

    feat: change collectCoverageFrom

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,7 +8,7 @@ import { AddressModule } from './address/address.module';
 import { CacheModule } from './cache/cache.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './guards/roles.guards';
+import { RolesGuard } from './guards/roles.guard';
 import { JwtModule } from '@nestjs/jwt';
 import { CategoryModule } from './category/category.module';
 import { ProductModule } from './product/product.module';
