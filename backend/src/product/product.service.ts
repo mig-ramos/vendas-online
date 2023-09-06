61a020024509e7a7b7ca42323c9ad327ea5e6016
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-09-05 21:43:34 -0300

    feat: insert amount product in categories

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,10 +1,16 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryService } from '../category/category.service';
 import { DeleteResult, In, Repository } from 'typeorm';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { ProductEntity } from './entities/product.entity';
 import { UpdateProductDTO } from './dtos/update-product.dto';
+import { CountProduct } from './dtos/count-product.dto';
 
 @Injectable()
 export class ProductService {
@@ -12,6 +18,7 @@ export class ProductService {
     @InjectRepository(ProductEntity)
     private readonly productRepository: Repository<ProductEntity>,
 
+    @Inject(forwardRef(() => CategoryService))
     private readonly categoryService: CategoryService,
   ) {}
 
@@ -39,6 +46,7 @@ export class ProductService {
     }
 
     const products = await this.productRepository.find(findOptions);
+
     if (!products || products.length === 0) {
       throw new NotFoundException('Not found products');
     }
@@ -84,5 +92,13 @@ export class ProductService {
       ...product,
       ...updateProduct,
     });
+  }
+
+  async countProdutsByCategoryId(): Promise<CountProduct[]> {
+    return this.productRepository
+      .createQueryBuilder('product')
+      .select('product.category_id, COUNT(*) as total')
+      .groupBy('product.category_id')
+      .getRawMany();
   }
 }
