c9276d242d1c36ee01d88bf2d12687f861bb288e
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-09-07 19:08:32 -0300

    feat: insert cityId in ReturnCep

--- backend/src/correios/correios.service.ts
+++ backend/src/correios/correios.service.ts
@@ -5,8 +5,10 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { AxiosError } from 'axios';
-import { CityService } from 'src/city/city.service';
+import { CityService } from '../city/city.service';
+import { CityEntity } from '../city/entities/city.entity';
 import { ReturnCepExternal } from './dtos/return-cep-external.dto';
+import { ReturnCep } from './dtos/return-cep.dto';
 
 @Injectable()
 export class CorreiosService {
@@ -16,7 +18,7 @@ export class CorreiosService {
     private readonly cityService: CityService,
   ) {}
 
-  async findAddressByCep(cep: string): Promise<ReturnCepExternal> {
+  async findAddressByCep(cep: string): Promise<ReturnCep> {
     const returnCep: ReturnCepExternal = await this.httpService.axiosRef
       .get<ReturnCepExternal>(this.URL_CORREIOS.replace('{CEP}', cep))
       .then((result) => {
@@ -31,13 +33,10 @@ export class CorreiosService {
         );
       });
 
-    const city = await this.cityService.findCityByName(
-      returnCep.localidade,
-      returnCep.uf,
-    );
+    const city: CityEntity | undefined = await this.cityService
+      .findCityByName(returnCep.localidade, returnCep.uf)
+      .catch(() => undefined);
 
-    console.log('city', city);
-
-    return returnCep;
+    return new ReturnCep(returnCep, city?.id, city?.state?.id);
   }
 }
