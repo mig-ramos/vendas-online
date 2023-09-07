94be5e67cbd99a7233ba58b36ccd4d63bfdf7a45
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-09-07 20:37:40 -0300

    feat: insert connection soap in project

--- backend/src/correios/correios.controller.ts
+++ backend/src/correios/correios.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CorreiosService } from './correios.service';
+import { ResponsePriceCorreios } from './dtos/response-price-correios';
 import { ReturnCep } from './dtos/return-cep.dto';
 
 @Controller('correios')
 export class CorreiosController {
   constructor(private readonly correiosService: CorreiosService) {}
+
+  @Get('/price')
+  async priceDelivery(): Promise<ResponsePriceCorreios> {
+    return this.correiosService.priceDelivery();
+  }
 
   @Get('/:cep')
   async findAll(@Param('cep') cep: string): Promise<ReturnCep> {
